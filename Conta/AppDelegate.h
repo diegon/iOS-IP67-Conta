6143473e8ee8be7486b4e0c0c9771953d4bf07da
Diego Negrelli <diegon@gmail.com>
2015-09-05 13:45:05 -0300

    Initial Commit

---
+++ Conta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Conta
+//
+//  Created by ios5380 on 05/09/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
